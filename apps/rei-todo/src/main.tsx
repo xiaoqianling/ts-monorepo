f8160b91d9e7678e22487bcf8fbd67d0c4e5aa5d
zhaoyu.rei <zhaoyu.rei@bytedance.com>
2024-12-23 15:01:39 +0800

    chore: mac初始化

--- apps/rei-todo/src/main.tsx
+++ apps/rei-todo/src/main.tsx
@@ -1,13 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ReiButton } from "@rei/design/button";
-import App from "./App";
-import "./index.css";
+import { ReiButton } from "rei-design/button";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     {/* <App /> */}
     <ReiButton />
-    <Button />
   </React.StrictMode>
 );
